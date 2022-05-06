aaca1e9b798816ab77ad872b314d811447e877e0
Chaicow <100246644+Chaicow@users.noreply.github.com>
2022-05-06 16:13:00 -0700

    speed

--- R/ridge_regression.R
+++ R/ridge_regression.R
@@ -82,7 +82,8 @@ find_best_lambda <- function(train_dat, test_dat, response, lambda) {
 
     betas <- t(solve(t(x) %*% x + lambda * diag(ncol(x))) %*% (t(x) %*% y))
     preds <- x %*% t(betas)
-    results <- data.table(cbind(test, preds))
+    results <- cbind(test, preds) %>%
+      data.table()
 
     sse <- results %>%
         mutate(sqr_error = ((test - preds)^2)) %>%
